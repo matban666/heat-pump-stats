8d4d0ab2ceade7412e4129cd31809199bce7c7ef
Matt Banbury <matt.banbury@sysmech.co.uk>
2024-10-13 12:41:50 +0100

    Added a data ingestor and moved data type creation out of the datatypes class to make the influx loading more re-usable

---
+++ src/datasource/data_ingestor.py
@@ -0,0 +1,46 @@
+
+from utils.time import ceil_dt
+from abc import ABC, abstractmethod
+
+class DataIngestor(ABC):
+    @abstractmethod
+    def add_data(self, time, name, value):
+        """
+        Implement this in derived class.  It is called by somthing that loads data.  
+        This separates the method of loading data from the method of storing it.
+        """
+        raise NotImplementedError
+
+class DataByTime(DataIngestor):
+    def __init__(self, local_timezone):
+        """
+        Initializes the heat pump object.
+        Parameters:
+        - local_timezone (str): The local timezone of the heat pump.
+        Returns:
+        None
+        """
+        self.local_timezone = local_timezone
+        self._data = {}
+
+    def add_data(self, time, name, value):
+        """
+        Called when a data item is loaded.
+        Parameters:
+        - time (str): The time at which the data is recorded.
+        - name (str): The name of the data.
+        - value (float): The value of the data.
+        Returns:
+        None
+        """
+        time_rounded = ceil_dt(time)
+
+        local_time = time_rounded.astimezone(self.local_timezone)
+
+        if local_time in self._data:
+            self._data[local_time][name] = value
+        else:
+            self._data[local_time] = {name: value}
+
+    def get_data(self):
+        return self._data
